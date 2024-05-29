e7c29525ebad77ce26f5db121d4c39a7fb11076b
markuskrash <markgrechnev@gmail.com>
2024-05-30 02:18:05 +0300

    финальный проект

---
+++ app/api/api-hooks.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import {getNormalizedGamesDataByCategory, isResponseOk} from './api-utils'
+
+export const useGetDataByCategory = (endpoint, category) => {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getNormalizedGamesDataByCategory(endpoint, category)
+      setData(data)
+    }
+    fetchData()
+  }, [])
+  if(isResponseOk(data)) return data;
+}
